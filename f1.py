a963f2f879319d359581a323f6dabeef3b774dc6
sadegh ipaki <sadegh.6444@gmail.com>
2019-06-04 05:10:03 -0400

    second commit added f1.py

---
+++ f1.py
@@ -0,0 +1,13 @@
+
+l = []
+w = ''
+def f(w,c,l,i,size,parameter):
+	w += c
+	if i == size :
+		l = l.append(w)
+		return w
+	for j in range(parameter):
+		f(w,str(j),l,i+1,size,parameter)
+
+f(w,'',l,0,3,3)
+print(l)
